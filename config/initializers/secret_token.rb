435018ffd8823fec4f20e560a026b31c6e23e06f
Luis Arias <luisantonioa@gmail.com>
2013-08-18 15:23:52 -0700

    Initial rails 4 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dom3mapgen::Application.config.secret_key_base = '78380d4081fc499331537d91b319df42d81afaddd2bbe29506971e7cb7dcb927df1c05690f6679130600fe29a3d76c88a8dea915b20929c574437fa0396aca91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dom3mapgen::Application.config.session_store :cookie_store, key: '_dom3mapgen_session'
